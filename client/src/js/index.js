c0d94bc60809932872c828c648f19f3a65c667a0
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-09-15 20:07:55 -0400

    added indexDB

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -3,6 +3,7 @@ import "./submit"
 
 import { Tooltip, Toast, Popover } from "bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { initDb } from "./database"
 
 import "../css/index.css"
 
@@ -11,6 +12,7 @@ import Bear from "../images/bear.png"
 import Dog from "../images/dog.png"
 
 window.addEventListener('load', function () {
+    initDb()
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
