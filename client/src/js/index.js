dc2481e15fecfe13467b04336df15edea7da3ae9
jakem8532 <85272755+jakem8532@users.noreply.github.com>
2022-09-17 12:59:29 -0400

    added installable functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -12,6 +12,8 @@ import Logo from "../images/logo.png"
 import Bear from "../images/bear.png"
 import Dog from "../images/dog.png"
 
+const installBtn = document.getElementById('installBtn')
+
 window.addEventListener('load', function () {
     initDb()
     fetchCards()
@@ -85,3 +87,19 @@ window.addEventListener('load', function () {
         navigator.serviceWorker.register('./service-worker.js')
     })
   }
+
+  window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault()
+
+    installBtn.style.visibility = 'visible'
+
+    installBtn.addEventListener('click', () => {
+        event.prompt()
+        installBtn.setAttribute('disabled', true)
+        installBtn.textContent = 'Installed!'
+    })
+  })
+
+  window.addEventListener('appinstalled', (event) => {
+    console.log('app installed', event)
+  })
